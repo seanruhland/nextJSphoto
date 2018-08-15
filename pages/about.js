6615027cc2e5c0eb5eb8f03a6bbaf7404bb2c702
Sean Ruhland <sean.ruhland@gmail.com>
2018-08-15 12:11:48 -0700

    scss file importing and postcss autoprefixing config

---
+++ pages/about.js
@@ -0,0 +1,20 @@
+import Navbar from '../components/Navbar'
+
+const About = () => (
+
+  <section>
+  <Navbar />
+    <h1>About Next JS</h1>
+    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam nec pellentesque metus, ac mollis erat. Vivamus at facilisis quam, in vehicula erat. Aenean orci ante, pellentesque nec vehicula posuere, dignissim sed tortor. Ut dapibus sagittis aliquet. Duis id dolor sed sem imperdiet fringilla. In non egestas sem, ut consequat elit. Aenean iaculis nunc vitae ante laoreet efficitur. Cras blandit volutpat ultrices.
+
+Donec volutpat faucibus felis, id placerat purus consequat id. Suspendisse at diam ac diam convallis sollicitudin in nec eros. Nullam facilisis ex tristique felis luctus viverra. Nullam consectetur at erat sit amet luctus. Donec a felis augue. Quisque nec pretium turpis, in fermentum risus. Etiam ac metus a mauris mattis scelerisque. Integer sed urna volutpat, consequat nibh at, ornare felis. Mauris blandit ut turpis a congue. Pellentesque vestibulum tincidunt enim varius vehicula. Nullam faucibus quis mi ac bibendum. Duis luctus lacus volutpat dui gravida, eget ornare ex iaculis. Ut accumsan leo leo, eu scelerisque quam blandit nec. Cras consectetur eu nunc ut ornare. Nullam vitae urna et nulla finibus dictum mattis quis sem.</p>
+    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam nec pellentesque metus, ac mollis erat. Vivamus at facilisis quam, in vehicula erat. Aenean orci ante, pellentesque nec vehicula posuere, dignissim sed tortor. Ut dapibus sagittis aliquet. Duis id dolor sed sem imperdiet fringilla. In non egestas sem, ut consequat elit. Aenean iaculis nunc vitae ante laoreet efficitur. Cras blandit volutpat ultrices.
+
+Donec volutpat faucibus felis, id placerat purus consequat id. Suspendisse at diam ac diam convallis sollicitudin in nec eros. Nullam facilisis ex tristique felis luctus viverra. Nullam consectetur at erat sit amet luctus. Donec a felis augue. Quisque nec pretium turpis, in fermentum risus. Etiam ac metus a mauris mattis scelerisque. Integer sed urna volutpat, consequat nibh at, ornare felis. Mauris blandit ut turpis a congue. Pellentesque vestibulum tincidunt enim varius vehicula. Nullam faucibus quis mi ac bibendum. Duis luctus lacus volutpat dui gravida, eget ornare ex iaculis. Ut accumsan leo leo, eu scelerisque quam blandit nec. Cras consectetur eu nunc ut ornare. Nullam vitae urna et nulla finibus dictum mattis quis sem.</p>
+    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam nec pellentesque metus, ac mollis erat. Vivamus at facilisis quam, in vehicula erat. Aenean orci ante, pellentesque nec vehicula posuere, dignissim sed tortor. Ut dapibus sagittis aliquet. Duis id dolor sed sem imperdiet fringilla. In non egestas sem, ut consequat elit. Aenean iaculis nunc vitae ante laoreet efficitur. Cras blandit volutpat ultrices.
+
+Donec volutpat faucibus felis, id placerat purus consequat id. Suspendisse at diam ac diam convallis sollicitudin in nec eros. Nullam facilisis ex tristique felis luctus viverra. Nullam consectetur at erat sit amet luctus. Donec a felis augue. Quisque nec pretium turpis, in fermentum risus. Etiam ac metus a mauris mattis scelerisque. Integer sed urna volutpat, consequat nibh at, ornare felis. Mauris blandit ut turpis a congue. Pellentesque vestibulum tincidunt enim varius vehicula. Nullam faucibus quis mi ac bibendum. Duis luctus lacus volutpat dui gravida, eget ornare ex iaculis. Ut accumsan leo leo, eu scelerisque quam blandit nec. Cras consectetur eu nunc ut ornare. Nullam vitae urna et nulla finibus dictum mattis quis sem.</p>
+  </section>
+)
+
+  export default About;
---
+++ pages/index.js
@@ -0,0 +1,14 @@
+import Navbar from '../components/Navbar'
+import "../scss/style.scss"
+
+
+
+const Index = () => (
+  <section>
+  <Navbar />
+    <h1>Hello World From Next JS</h1>
+
+  </section>
+  )
+
+export default Index;
